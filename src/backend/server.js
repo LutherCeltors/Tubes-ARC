98183ef137eb0051fb4eee5b93bdf4f784d881cb
Marcel <13524063@mahasiswa.itb.ac.id>
2025-05-31 13:48:30 +0700

    feat: adding conversion functionality and API

--- src/backend/server.js
+++ src/backend/server.js
@@ -6,10 +6,7 @@ const PORT = 5000;
 
 app.use(logger);
 app.use('/', express.static(path.join(__dirname, '../frontend'))); //conecting to frontend
-
-app.get('/', async (req, res) => {
-
-});
+app.use(express.json());
 
 app.get("/", (req, res) =>{
     res.sendFile(path.join(__dirname, 'frontend'))
@@ -32,9 +29,30 @@ app.get("/grafik", async (req, res) =>{
     }
 });
 
-app.get("/konversi", (req, res) =>{
-    res.send("konversi")
+app.get("/api/conversion", async (req, res) =>{
     console.log("konversi")
+    const {fromCurr = 'IDR', toCurr = 'EUR'} = req.query;
+    try {
+        const response = await axios.get(`https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_fd18nyAhD1Xsl9y0jhqsGWM4CwMCr7tDnDZcnHyG&currencies=${toCurr}&base_currency=${fromCurr}`);
+    
+        const externalData = response.data.data;
+    
+        console.log("Ambil data dari API");
+
+        const rate = externalData[toCurr];
+        
+        if (!rate) {
+        return res.status(400).json({ error: "Invalid currency code" });
+        }
+
+        res.json({
+            from: fromCurr ,
+            to: toCurr,
+            rate
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch external chart data' });
+    }
 });
 
 app.listen(PORT, () =>{
